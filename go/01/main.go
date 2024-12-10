b135eb94a56d739d54cbd31c89dc96a6411a91f1
Spencer Puthuff <13261923+evilputty@users.noreply.github.com>
2024-12-09 19:27:54 -0800

    initial loading of data

--- go/01/main.go
+++ go/01/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("first challenge")
+	// pull data from input file
+	// split from "   "
+	// convert to integers and sort numerically
+	// add difference of lowest integers for each line consecutively for answer
+
+	file, err := os.Open("input_data.txt")
+	if err != nil {
+		fmt.Printf("Error Reading File: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		get_answer(strings.Split(scanner.Text(), "   "))
+	}
+}
+
+func get_answer(line []string) int {
+	// split the string data coming in like "12345   12345"
+	for i, line := range line{
+		num, err := strconv.Atoi(line)
+		if err != nil {}
+			fmt.Println("Error Converting to integer", err)
+			continue
+		}
+		nums[i] = num
+	}
+
+	
+	var answer int // placeholder
+	return answer
 }
